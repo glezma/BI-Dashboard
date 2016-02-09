a3e40a26821a15bacb6c414bf83de6bd60c93734
Gonzalo Lezma <gonzalolezma@gmail.com>
2016-02-08 22:04:53 -0500

    update2

--- server.py
+++ server.py
@@ -58,7 +58,8 @@ def process():
 
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=False)
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port,debug=True)
 
 	
